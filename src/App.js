f668e754d20026674edbadf0d42a1fd86494a435
JP Hechter <jhechter@fearless.tech>
2021-11-01 13:40:23 -0400

    moved ga to index.html in hopes that fixes tracking issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { Helmet } from 'react-helmet';
 import './App.css';
 import Calculator from './Calculator';
 import Conversion from './Conversion';
@@ -10,18 +9,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* TODO: There's a better way to do this */}
-      {window.location.host === "coorscalc.com" ?
-      <Helmet>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FRJPP2VX4L"></script>
-        <script 
-        dangerouslySetInnerHTML={`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-FRJPP2VX4L');`}
-        />
-      </Helmet> : null}
       <h1>THE COORS CALC<sup>®</sup></h1>
       <Conversion value={userInput}/>
       <Calculator input={(userInput) => setInput(userInput)}/>
